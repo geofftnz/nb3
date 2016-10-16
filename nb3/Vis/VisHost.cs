b4c0dcc22afa1da23532496737273dfe281f9216
Geoff Thornburrow <geofftnz@gmail.com>
2016-10-16 22:17:13 +1300

    clear to blue background

--- nb3/Vis/VisHost.cs
+++ nb3/Vis/VisHost.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using System.Threading;
 
 namespace nb3.Vis
 {
@@ -57,12 +59,33 @@ namespace nb3.Vis
 
         private void VisHost_RenderFrame(object sender, FrameEventArgs e)
         {
+            GL.ClearColor(0.0f, 0.1f, 0.4f, 1.0f);
+            GL.ClearDepth(1.0);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            SwapBuffers();
         }
 
         private void VisHost_UpdateFrame(object sender, FrameEventArgs e)
         {
+            Thread.Sleep(0);
+        }
 
+        private void SetProjection()
+        {
+            GL.Viewport(this.ClientRectangle);
+            SetOverlayProjection();
+            
+            //SetGBufferCombineProjection();
+        }
+
+        private void SetOverlayProjection()
+        {
+            //this.overlayProjection = Matrix4.CreateOrthographicOffCenter(0.0f, (float)this.ClientRectangle.Width / (float)this.ClientRectangle.Height, 1.0f, 0.0f, 0.001f, 10.0f);
+            //this.overlayModelview = Matrix4.Identity * Matrix4.CreateTranslation(0.0f, 0.0f, -1.0f);
+
+            //this.textManager.Projection = this.overlayProjection;
+            //this.textManager.Modelview = this.overlayModelview;
         }
     }
 }
