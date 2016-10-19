7338a799bf688af1f88ac27158661a9933fb30ef
Geoff Thornburrow <geofftnz@gmail.com>
2016-10-19 22:31:48 +1300

    Fonts, text on screen

--- nb3/Vis/VisHost.cs
+++ nb3/Vis/VisHost.cs
@@ -6,11 +6,22 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL4;
 using System.Threading;
+using OpenTKExtensions;
+using OpenTKExtensions.Framework;
+using OpenTKExtensions.Text;
 
 namespace nb3.Vis
 {
     public class VisHost : GameWindow
     {
+        private GameComponentCollection components = new GameComponentCollection();
+        private Font font;
+        private TextManager text;
+
+        private TextBlock title = new TextBlock("t0", "NeuralBeat3 ©2016 Geoff Thornburrow", new Vector3(0.0f, 0.05f, 0f), 0.0005f, new Vector4(1f, 0.8f, 0.2f, 1f));
+        private Matrix4 overlayProjection;
+        private Matrix4 overlayModelview;
+
         public VisHost()
             : base(800, 600,
                  new OpenTK.Graphics.GraphicsMode(
@@ -18,8 +29,8 @@ namespace nb3.Vis
                     "NB3",
                     GameWindowFlags.Default,
                     DisplayDevice.Default,
-                    3,3,
-                    OpenTK.Graphics.GraphicsContextFlags.Default
+                    4,0,
+                    OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible
                  )
         {
             this.VSync = VSyncMode.Off;
@@ -31,6 +42,13 @@ namespace nb3.Vis
             this.Resize += VisHost_Resize;
             this.Closed += VisHost_Closed;
             this.Closing += VisHost_Closing;
+
+            // create components
+            //components.Add(font = new Font(@"res\font\calibrib.ttf_sdf.2048.png", @"res\font\calibrib.ttf_sdf.2048.txt"), 1);
+            components.Add(font = new Font(@"res\font\lucon.ttf_sdf.1024.png", @"res\font\lucon.ttf_sdf.1024.txt"), 1);
+            components.Add(text = new TextManager(), 2);
+            font.Loaded += (s, e) => { text.Font = font; };
+
         }
 
         private void VisHost_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -40,34 +58,46 @@ namespace nb3.Vis
 
         private void VisHost_Closed(object sender, EventArgs e)
         {
-            
+
         }
 
         private void VisHost_Resize(object sender, EventArgs e)
         {
-            
+            SetProjection();
+            components.Resize(ClientRectangle.Width, ClientRectangle.Height);
         }
 
         private void VisHost_Unload(object sender, EventArgs e)
         {
+            components.Unload();
         }
 
         private void VisHost_Load(object sender, EventArgs e)
         {
+            // create components
 
+            components.Load();
+            SetProjection();
         }
 
         private void VisHost_RenderFrame(object sender, FrameEventArgs e)
         {
+            text.AddOrUpdate(title);
+
             GL.ClearColor(0.0f, 0.1f, 0.4f, 1.0f);
             GL.ClearDepth(1.0);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Disable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend);
+
+            text.Render();
 
             SwapBuffers();
         }
 
         private void VisHost_UpdateFrame(object sender, FrameEventArgs e)
         {
+
             Thread.Sleep(0);
         }
 
@@ -75,17 +105,17 @@ namespace nb3.Vis
         {
             GL.Viewport(this.ClientRectangle);
             SetOverlayProjection();
-            
+
             //SetGBufferCombineProjection();
         }
 
         private void SetOverlayProjection()
         {
-            //this.overlayProjection = Matrix4.CreateOrthographicOffCenter(0.0f, (float)this.ClientRectangle.Width / (float)this.ClientRectangle.Height, 1.0f, 0.0f, 0.001f, 10.0f);
-            //this.overlayModelview = Matrix4.Identity * Matrix4.CreateTranslation(0.0f, 0.0f, -1.0f);
+            overlayProjection = Matrix4.CreateOrthographicOffCenter(0.0f, (float)this.ClientRectangle.Width / (float)this.ClientRectangle.Height, 1.0f, 0.0f, 0.001f, 10.0f);
+            overlayModelview = Matrix4.Identity * Matrix4.CreateTranslation(0.0f, 0.0f, -1.0f);
 
-            //this.textManager.Projection = this.overlayProjection;
-            //this.textManager.Modelview = this.overlayModelview;
+            this.text.Projection = this.overlayProjection;
+            this.text.Modelview = this.overlayModelview;
         }
     }
 }
