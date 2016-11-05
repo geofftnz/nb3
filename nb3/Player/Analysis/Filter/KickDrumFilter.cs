e4eadbb892388550a33616ffb4180593e7441a32
Geoff Thornburrow <geofftnz@gmail.com>
2016-11-05 21:16:03 +1300

    kickdrum filter tweaks

--- nb3/Player/Analysis/Filter/KickDrumFilter.cs
+++ nb3/Player/Analysis/Filter/KickDrumFilter.cs
@@ -9,16 +9,21 @@ namespace nb3.Player.Analysis.Filter
 {
     public class KickDrumFilter : ISpectrumFilter
     {
-        private const int NUMOUTPUTS = 1;
+        private const int NUMOUTPUTS = 4;
         public int OutputOffset { get; set; }
         public int OutputSlots { get { return NUMOUTPUTS; } }
         private float[] outputs = new float[NUMOUTPUTS];
 
+        public float Threshold { get; set; } = 0.2f;
+        public float Decay { get; set; } = 0.02f;
+        public float Release { get; set; } = 0.2f;
+        
 
         private int freqStart, freqCount;
         private float lowpassCoeff;
         private float avg = 0f;
-
+        private float activation = 1f;
+        
 
         public KickDrumFilter(int freq_start = 0, int freq_count = 8, float lowpass_coeff = 0.98f)
         {
@@ -37,11 +42,24 @@ namespace nb3.Player.Analysis.Filter
             }
             current /= (float)freqCount;
             current = current.NormDB();
+            outputs[0] = current;
 
             avg = avg * lowpassCoeff + current * (1f - lowpassCoeff);
+            outputs[1] = avg;
 
-            outputs[0] = Math.Max(0f, (current - avg) * 4.0f);
+            avg = Math.Max(0.3f, avg);
 
+            outputs[2] = Math.Max(0f, (current - avg) * 4.0f);
+
+            activation += Decay;
+            activation = Math.Min(1f, activation);
+
+            if (outputs[2] > Threshold && activation > Release)
+            {
+                activation = 0f;
+            }
+
+            outputs[3] = 1f - activation;
 
             return outputs;
         }
