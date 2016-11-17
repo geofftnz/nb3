cddd12d3bb1ad920921d76e73f663d0bfd0e553c
Geoff Thornburrow <geofftnz@gmail.com>
2016-11-17 22:06:19 +1300

    peak freq filter

--- nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
+++ nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
@@ -24,25 +24,69 @@ namespace nb3.Player.Analysis.Filter
 
         public int FreqStart { get; set; }
         public int FreqCount { get; set; }
+        public float FreqSensitivity { get; set; } = 0.01f;
+
+        private float[] LowPassSpectrum = new float[Globals.SPECTRUMRES];
+        private float[] SmoothSpectrum = new float[Globals.SPECTRUMRES];
+
+        private float[] FilterKernel = new float[] { 0.0093f, 0.028002f, 0.065984f, 0.121703f, 0.175713f, 0.198596f, 0.175713f, 0.121703f, 0.065984f, 0.028002f, 0.0093f };
+        private float prev_index;
 
         public PeakFrequencyFilter(int freq_start = 0, int freq_count = 8)
         {
             FreqStart = freq_start;
             FreqCount = freq_count;
+            prev_index = freq_count / 2;
+        }
+
+        private void GenerateLowPassSpectrum(float[] s, float k)
+        {
+            for (int i = 0; i < LowPassSpectrum.Length; i++)
+            {
+                LowPassSpectrum[i] = LowPassSpectrum[i] * k + (1f - k) * s[i];
+            }
+
+        }
+
+        private void GenerateSmoothSpectrum(float[] s, float[] k)
+        {
+            int filterSize = k.Length;
+            int filterRadius = filterSize / 2;
+
+            for (int i = 0; i < SmoothSpectrum.Length; i++)
+            {
+                float total = 0f;
+
+                for (int j = 0; j < filterSize; j++)
+                {
+                    int si = i + j - filterRadius;
+                    if (si < 0) si = 0;
+                    if (si >= s.Length) si = s.Length - 1;
+
+                    total += s[si] * k[j];
+                }
+                SmoothSpectrum[i] = total;
+            }
         }
 
         public float[] GetValues(FilterParameters frame)
         {
+            GenerateLowPassSpectrum(frame.SpectrumDB, 0.6f);
+
+            // Generate a smooth spectrum
+            GenerateSmoothSpectrum(LowPassSpectrum, FilterKernel);
 
             // 1: find the highest point on the normalized spectrum
             // calculate the average along the way
             float current = 0f, max = 0f;
             int index = 0;
             float total = 0f;
+            float distance;
 
             for (int i = 0; i < FreqCount; i++)
             {
-                current = frame.SpectrumDB[FreqStart + i];
+                distance = (float)Math.Abs(i - prev_index) * FreqSensitivity;
+                current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i]) / (1f + distance);
                 total += current;
                 if (current > max)
                 {
@@ -55,12 +99,16 @@ namespace nb3.Player.Analysis.Filter
             output[(int)FilterOutputs.Frequency] = (float)index / FreqCount;
             output[(int)FilterOutputs.AbsLevel] = max;
             output[(int)FilterOutputs.RelLevel] = (max - total) * 2f;
+            prev_index = index;
+            if (max < 0.1f)
+                prev_index = FreqCount / 2;
 
             return output;
         }
 
         public void Reset()
         {
+            prev_index = FreqCount / 2;
         }
     }
 }
