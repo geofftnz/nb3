f131ee9acc4a8ca267dfb600fd03e4a4b50c5749
Geoff Thornburrow <geofftnz@gmail.com>
2016-11-17 22:17:09 +1300

    peak freq filter

--- nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
+++ nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
@@ -24,13 +24,14 @@ namespace nb3.Player.Analysis.Filter
 
         public int FreqStart { get; set; }
         public int FreqCount { get; set; }
-        public float FreqSensitivity { get; set; } = 0.01f;
+        public float FreqSensitivity { get; set; } = 0.001f;
 
         private float[] LowPassSpectrum = new float[Globals.SPECTRUMRES];
         private float[] SmoothSpectrum = new float[Globals.SPECTRUMRES];
 
         private float[] FilterKernel = new float[] { 0.0093f, 0.028002f, 0.065984f, 0.121703f, 0.175713f, 0.198596f, 0.175713f, 0.121703f, 0.065984f, 0.028002f, 0.0093f };
         private float prev_index;
+        private float prev_max;
 
         public PeakFrequencyFilter(int freq_start = 0, int freq_count = 8)
         {
@@ -85,8 +86,9 @@ namespace nb3.Player.Analysis.Filter
 
             for (int i = 0; i < FreqCount; i++)
             {
-                distance = (float)Math.Abs(i - prev_index) * FreqSensitivity;
-                current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i]) / (1f + distance);
+                distance = (float)Math.Abs(i - prev_index) * (FreqSensitivity + prev_max * 0.01f);
+                //current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i]) / (1f + distance);
+                current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i] * 0.5f) / (1f + distance);
                 total += current;
                 if (current > max)
                 {
@@ -97,10 +99,12 @@ namespace nb3.Player.Analysis.Filter
             total /= FreqCount;
 
             output[(int)FilterOutputs.Frequency] = (float)index / FreqCount;
-            output[(int)FilterOutputs.AbsLevel] = max;
+            output[(int)FilterOutputs.AbsLevel] = max * 4f;
             output[(int)FilterOutputs.RelLevel] = (max - total) * 2f;
+
+            prev_max = max;
             prev_index = index;
-            if (max < 0.1f)
+            if (max < 0.02f)
                 prev_index = FreqCount / 2;
 
             return output;
@@ -109,6 +113,7 @@ namespace nb3.Player.Analysis.Filter
         public void Reset()
         {
             prev_index = FreqCount / 2;
+            prev_max = 0f;
         }
     }
 }
