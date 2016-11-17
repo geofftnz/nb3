2e6349928c26e6f712b57a93be44970488966f52
Geoff Thornburrow <geofftnz@gmail.com>
2016-11-17 23:09:19 +1300

    peak freq filter tweaks

--- nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
+++ nb3/Player/Analysis/Filter/PeakFrequencyFilter.cs
@@ -9,13 +9,16 @@ namespace nb3.Player.Analysis.Filter
 {
     public class PeakFrequencyFilter : ISpectrumFilter
     {
-        private const int NUMOUTPUTS = 3;
+        private const int NUMOUTPUTS = 5;
 
         public enum FilterOutputs
         {
             Frequency = 0,
             AbsLevel,
-            RelLevel
+            RelLevel,
+            MaxLowpass,
+            Selectivity
+
         }
 
         public int OutputOffset { get; set; }
@@ -32,12 +35,15 @@ namespace nb3.Player.Analysis.Filter
         private float[] FilterKernel = new float[] { 0.0093f, 0.028002f, 0.065984f, 0.121703f, 0.175713f, 0.198596f, 0.175713f, 0.121703f, 0.065984f, 0.028002f, 0.0093f };
         private float prev_index;
         private float prev_max;
+        private float index_lowpass;
+        private float max_lowpass = 0f;
 
         public PeakFrequencyFilter(int freq_start = 0, int freq_count = 8)
         {
             FreqStart = freq_start;
             FreqCount = freq_count;
             prev_index = freq_count / 2;
+            index_lowpass = prev_index;
         }
 
         private void GenerateLowPassSpectrum(float[] s, float k)
@@ -72,7 +78,7 @@ namespace nb3.Player.Analysis.Filter
 
         public float[] GetValues(FilterParameters frame)
         {
-            GenerateLowPassSpectrum(frame.SpectrumDB, 0.6f);
+            GenerateLowPassSpectrum(frame.SpectrumDB, 0.4f);
 
             // Generate a smooth spectrum
             GenerateSmoothSpectrum(LowPassSpectrum, FilterKernel);
@@ -84,9 +90,11 @@ namespace nb3.Player.Analysis.Filter
             float total = 0f;
             float distance;
 
+            float selectivity = (FreqSensitivity + max_lowpass * 0.01f);
+
             for (int i = 0; i < FreqCount; i++)
             {
-                distance = (float)Math.Abs(i - prev_index) * (FreqSensitivity + prev_max * 0.01f);
+                distance = (float)Math.Abs(i - prev_index) * selectivity;
                 //current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i]) / (1f + distance);
                 current = Math.Max(0f, LowPassSpectrum[FreqStart + i] - SmoothSpectrum[FreqStart + i] * 0.5f) / (1f + distance);
                 total += current;
@@ -98,9 +106,14 @@ namespace nb3.Player.Analysis.Filter
             }
             total /= FreqCount;
 
-            output[(int)FilterOutputs.Frequency] = (float)index / FreqCount;
-            output[(int)FilterOutputs.AbsLevel] = max * 4f;
+            index_lowpass = index_lowpass * 0.3f + 0.7f * ((float)index / (float)FreqCount);
+            max_lowpass = max_lowpass * 0.9f + 0.1f * max;
+
+            output[(int)FilterOutputs.Frequency] = (float)Math.Sqrt(index_lowpass);
+            output[(int)FilterOutputs.AbsLevel] = max;
             output[(int)FilterOutputs.RelLevel] = (max - total) * 2f;
+            output[(int)FilterOutputs.MaxLowpass] = max_lowpass;
+            output[(int)FilterOutputs.Selectivity] = selectivity * 100f;
 
             prev_max = max;
             prev_index = index;
@@ -114,6 +127,7 @@ namespace nb3.Player.Analysis.Filter
         {
             prev_index = FreqCount / 2;
             prev_max = 0f;
+            index_lowpass = prev_index;
         }
     }
 }
--- nb3/Player/Analysis/SpectrumAnalyser.cs
+++ nb3/Player/Analysis/SpectrumAnalyser.cs
@@ -30,7 +30,7 @@ namespace nb3.Player.Analysis
             filterParams.SpectrumHistory = SpectrumHistory;
 
             // The order of these is important
-            AddFilter(new PeakFrequencyFilter(0, Globals.SPECTRUMRES/4));
+            AddFilter(new PeakFrequencyFilter(0, Globals.SPECTRUMRES/3));
             AddFilter(new KickDrumFilter3(0, 6));
             AddFilter(new KickDrumFilter2(0, 4));
             AddFilter(new KickDrumFilter2(0, 8));
