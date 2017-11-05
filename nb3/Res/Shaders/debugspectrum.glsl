80ee251441095e301d8de57462cba34849b87302
Geoff Thornburrow <geofftnz@gmail.com>
2017-11-05 22:11:50 +1300

    tweaks

--- nb3/Res/Shaders/debugspectrum.glsl
+++ nb3/Res/Shaders/debugspectrum.glsl
@@ -59,6 +59,12 @@ vec3 colscale(float s)
 	return col;
 }
 
+vec3 colscale_bw(float s)
+{
+	return vec3(s*s*s*s*4.0);
+}
+
+
 //|spectrum_common
 vec2 texel = vec2(1.0/1024.0,0.0);
 
@@ -66,12 +72,56 @@ vec4 getSample(sampler2D spectrum, vec2 t)
 {
 	vec2 raw = texture2D(spectrum,t).rg;
 
+	// TODO: potentially don't need this
 	float sep = asin((raw.g - raw.r) / (raw.g + raw.r)) / PIOVER2;
 
 	vec4 s = vec4(raw.rg,(raw.r+raw.g)*0.5, sep);
 
 	return s;
 }
+
+vec4 getSample_sharpen(sampler2D spectrum, vec2 t)
+{
+	vec2 raw = vec2(0.0);
+
+	/*
+	raw += texture2D(spectrum,t + texel*4.0).rg * -0.0625;
+	raw += texture2D(spectrum,t + texel*3.0).rg * -0.125;
+	raw += texture2D(spectrum,t + texel*2.0).rg * -0.25;
+	raw += texture2D(spectrum,t + texel).rg * -0.5;
+	raw += texture2D(spectrum,t).rg * 2.0;
+	raw += texture2D(spectrum,t - texel).rg * -0.5;
+	raw += texture2D(spectrum,t - texel*2.0).rg * -0.25;
+	raw += texture2D(spectrum,t - texel*3.0).rg * -0.125;
+	raw += texture2D(spectrum,t - texel*4.0).rg * -0.0625;
+	*/
+
+	raw = texture2D(spectrum,t).rg * 2.0;
+
+	vec2 background = vec2(0.0);
+	float ofs = 1.0;
+	for(int i=0;i<8;i++){
+
+		background += texture2D(spectrum,t - texel * ofs).rg / (ofs*0.7 + 1.0);
+		background += texture2D(spectrum,t + texel * ofs).rg / (ofs*0.7 + 1.0);
+
+		ofs += 1.0;
+	}
+
+	//background /= 10.0;
+	raw -= background * 0.9;
+
+
+	raw = max(vec2(0.0),raw);
+
+	// TODO: potentially don't need this
+	float sep = asin((raw.g - raw.r) / (raw.g + raw.r)) / PIOVER2;
+
+	vec4 s = vec4(raw.rg,(raw.r+raw.g)*0.5, sep);
+
+	return s;
+}
+
 
 vec4 scaleSpectrum(vec4 s)
 {
@@ -102,15 +152,29 @@ vec4 renderSpectrum(vec2 t)
 	t.x = fscale(t.x);
 
 	// spectrum
-	vec3 col = colscale(scaleSpectrum(getSample(spectrumTex,t)).b);
+	//vec3 col = colscale(scaleSpectrum(getSample(spectrumTex,t)).b);
+
+	//vec4 samp = scaleSpectrum(getSample(spectrumTex,t));
+	vec4 samp = scaleSpectrum(getSample_sharpen(spectrumTex,t));
+
+	// normal
+	vec3 col = colscale(samp.b);
+
+	// stereo diff
+	//vec3 col_l = colscale_bw(samp.r * 1.5 - samp.g * 0.5) * vec3(1.0,0.5,0.0);
+	//vec3 col_r = colscale_bw(samp.g * 1.5 - samp.r * 0.5) * vec3(0.0,0.5,1.0);
+	//vec3 col = (col_l + col_r);
 
 	// stereo separation
 	//float s = todB(getSample(spectrumTex,t)).a;
-	//vec3 col = mix(vec3(1.0,0.0,0.0), vec3(0.0,0.0,1.0), s*0.5+0.5);
+	//vec3 col = mix(vec3(1.0,0.8,0.0), vec3(0.0,0.2,1.0), s*0.5+0.5);
 
+
+	// decoding position indicator
 	float a = 1.0 - smoothstep(abs(currentPosition-t.y),0.0,0.5/1024.0);
 	col += vec3(1.0,0.0,0.0) * a * 0.5;
 
+	// actual position indicator
 	float b = 1.0 - smoothstep(abs(currentPositionEst-t.y),0.0,0.5/1024.0);
 	col += vec3(0.0,1.0,0.0) * b * 0.5;
 
