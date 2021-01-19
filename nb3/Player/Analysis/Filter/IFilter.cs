7a07bd04dd47ed3ff331a058412dfcfa381c1e21
Geoff Thornburrow <geoff.thornburrow@xero.com>
2021-01-19 16:09:08 +1300

    IFilter

---
+++ nb3/Player/Analysis/Filter/IFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nb3.Player.Analysis.Filter
+{
+    /// <summary>
+    /// Simple filter interface. 
+    /// </summary>
+    public interface IFilter
+    {
+        float Get(float input);
+    }
+}
